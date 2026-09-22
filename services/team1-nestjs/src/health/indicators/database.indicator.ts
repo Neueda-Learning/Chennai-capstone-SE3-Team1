a3989982cfcfa7a0b14b6168de9b03fc3975a320
nadellaroshni <nadellaroshni@gmail.com>
2026-09-22 21:02:02 +0530

    Add health checks

--- services/team1-nestjs/src/health/indicators/database.indicator.ts
+++ services/team1-nestjs/src/health/indicators/database.indicator.ts
@@ -1,27 +1,14 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { HealthIndicatorResult, HealthCheckError } from '@nestjs/terminus';
 import { Pool } from 'pg';
 import { MultiFileLogger } from '../../logging/logger.service';
 
 @Injectable()
 export class DatabaseHealthIndicator {
-  private pool: Pool;
-
   constructor(
-    private readonly configService: ConfigService,
+    private readonly pool: Pool,
     @Inject('DATABASE_LOGGER') private readonly logger: MultiFileLogger,
-  ) {
-    this.pool = new Pool({
-      host: this.configService.get('app.database.host'),
-      port: this.configService.get('app.database.port'),
-      user: this.configService.get('app.database.username'),
-      password: this.configService.get('app.database.password'),
-      database: this.configService.get('app.database.name'),
-      connectionTimeoutMillis: 3000,
-      idleTimeoutMillis: 3000,
-    });
-  }
+  ) {}
 
   async isHealthy(key: string): Promise<HealthIndicatorResult> {
     try {
